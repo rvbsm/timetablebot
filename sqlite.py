692e96f7285e22733a230d4c96c0d9526beb2c76
rvbsm <mihailencko.r222@gmail.com>
2020-08-17 18:04:08 +0300

    sqlite3 file. added class_exist for classroom

--- sqlite.py
+++ sqlite.py
@@ -43,44 +43,53 @@ class SQLData:
 			for i in result:
 				return [i[0] for i in result]
 
+	def class_exist(self, user_id):
+		with self.connection:
+			try:
+				self.lock.acquire(True)
+				result = self.cursor.execute('''SELECT `classroom` FROM `users` WHERE `user_id` = ?''', (user_id,)).fetchall()
+				return bool(len(result))
+			finally:
+				self.lock.release()
+
 	def check_aclass(self, user_id):
 		with self.connection:
 			result = self.cursor.execute('''SELECT `aclassroom` FROM `users` WHERE `user_id` = ?''', (user_id,)).fetchall()
 			for i in result:
 				return [i[0] for i in result]
 
-	def mon(self):
+	def mon(self, clas):
 		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `monday`''').fetchall()
+			result = self.cursor.execute('''SELECT * FROM `monday` WHERE class = ?''', (clas,)).fetchall()
 			result = convert(result)
 			return result
-	def tue(self):
+	def tue(self, clas):
 		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `tuesday`''').fetchall()
-			result = convert(result)
-			return result
-
-	def wed(self):
-		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `wednesday`''').fetchall()
+			result = self.cursor.execute('''SELECT * FROM `tuesday` WHERE class = ?''', (clas,)).fetchall()
 			result = convert(result)
 			return result
 
-	def thur(self):
+	def wed(self, clas):
 		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `thursday`''').fetchall()
+			result = self.cursor.execute('''SELECT * FROM `wednesday` WHERE class = ?''', (clas,)).fetchall()
 			result = convert(result)
 			return result
 
-	def fri(self):
+	def thur(self, clas):
 		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `friday`''').fetchall()
+			result = self.cursor.execute('''SELECT * FROM `thursday` WHERE class = ?''', (clas,)).fetchall()
 			result = convert(result)
 			return result
 
-	def sat(self):
+	def fri(self, clas):
 		with self.connection:
-			result = self.cursor.execute('''SELECT * FROM `saturday`''').fetchall()
+			result = self.cursor.execute('''SELECT * FROM `friday` WHERE class = ?''', (clas,)).fetchall()
+			result = convert(result)
+			return result
+
+	def sat(self, clas):
+		with self.connection:
+			result = self.cursor.execute('''SELECT * FROM `saturday` WHERE class = ?''', (clas,)).fetchall()
 			result = convert(result)
 			return result
 
